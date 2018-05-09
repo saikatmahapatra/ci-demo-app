b9e122976e546463d57d4cfb76a5108f7600a830
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2018-05-09 23:39:54 +0530

    cart ui updated

--- application/views/site/_layouts/layout_default.php
+++ application/views/site/_layouts/layout_default.php
@@ -18,7 +18,7 @@
     </head>
 
     <body data-controller="<?php echo $this->router->class; ?>" data-method="<?php echo $this->router->method; ?>">
-        <nav class="navbar navbar-expand-md navbar-dark bg-indigo bg-primary fixed-top">
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
             <a class="navbar-brand" href="<?php echo site_url(); ?>"><?php echo $this->config->item('app_html_title'); ?></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
                 aria-expanded="false" aria-label="Toggle navigation">
@@ -30,7 +30,7 @@
             </div>
         </nav>
 		
-		<main role="main" class="container">
+		<main role="main" class="container-fluid">
             <?php echo $maincontent; ?>            			
         </main> <!-- /container -->
 		
--- application/views/site/order/my_cart.php
+++ application/views/site/order/my_cart.php
@@ -23,12 +23,14 @@
 </div>
 
 <div class="row">
-	<div class="col-md-8">
-		<?php echo form_open(current_url(), array('method' => 'post', 'class' => 'ci-form','name' => 'cartForm', 'id' => 'cartForm')); ?>
-		<?php echo form_hidden('form_action', 'update_cart'); ?>
+	<div class="col-12">
+	<?php echo form_open(current_url(), array('method' => 'post', 'class' => '','name' => 'cartForm', 'id' => 'cartForm')); ?>
+	<?php echo form_hidden('form_action', 'update_cart'); ?>
+	<div class="row">
+	<div class="col-md-8">		
 		<div class="card">
 			<div class="card-header">
-				My Cart (<?php echo $total_items; ?>)
+				Cart (<?php echo $total_items; ?>)
 			</div><!--/.card-header-->
 			<div class="card-body">				
 				<?php
@@ -37,31 +39,33 @@
 						foreach ($cartrows as $row) {									
 							//print_r($row);
 							?>
-							<div class="row cart-item" data-rowid="<?php echo $row['rowid']; ?>" data-id="<?php echo $row['id']; ?>">
-								<div class="col-md-12">
+							<div class="row cart-item mb-4 mt-4" data-rowid="<?php echo $row['rowid']; ?>" data-id="<?php echo $row['id']; ?>">
+								<div class="col-md-8">								
 									<div class="media">
 										<div class="mr-3 media-left media-top">
-										<img src="https://www.w3schools.com/bootstrap/img_avatar1.png" class="media-object" style="width:60px">
+										<img class="mr-3" data-src="holder.js/64x64" alt="64x64" style="width: 64px; height: 64px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2264%22%20height%3D%2264%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2064%2064%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16345c2a7ae%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16345c2a7ae%22%3E%3Crect%20width%3D%2264%22%20height%3D%2264%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2213.5546875%22%20y%3D%2236.5%22%3E64x64%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
 										</div>
 										<div class="media-body">											
-										<div class="mt-0"><?php echo $row['name']; ?></div>
-										<div>Size: M</div>
-										<div>Seller: Polo Store</div>
-										<div hidden><?php echo '<span class="currency" id="INR">&#8377;</span> ' . number_format($row['price'], 2); ?></div>
-										
-										<div class="row">
-											<div class="col-md-2">
-											<input name="quantity_<?php echo $row_counter; ?>" type="number" value="<?php echo $row['qty']; ?>" min="1" max="5" maxlength="2" class="form-control"/>
-											<?php echo form_hidden('rowid_'.$row_counter,$row['rowid']);?>
-											</div>
-											<div class="col-md-2">											
-											<a href="<?php echo site_url('order/remove_cart/'.$row['rowid']);?>" class="">REMOVE</a>
-											</div>
-											<div class="col-md-8 text-right text-bold"><?php echo '<span class="currency" id="INR">&#8377;</span>'.number_format($row['line_total'], 2); ?></div>
-										</div>												
-										</div>
+											<div class="mt-0"><h5><?php echo $row['name']; ?></h5></div>
+											<div>Display product attribute 1</div>
+											<div>Display product attribute 2</div>
+											<div>Display product attribute 3</div>
+											<div class="d-none"><?php echo '<span class="currency" id="INR">&#8377;</span> ' . number_format($row['price'], 2); ?></div>
+											<a href="<?php echo site_url('order/remove_cart/'.$row['rowid']);?>" class="">Remove</a>
+										</div><!--/.media-body-->
 									</div><!--/.media-->
 								</div>
+								<div class="col-md-2">												
+									<!--<input name="quantity_<?php echo $row_counter; ?>" type="number" value="<?php echo $row['qty']; ?>" min="1" max="5" maxlength="2" class="form-control"/>-->
+									<?php
+									$arr_quantity = array('1'=>'1','2'=>'2','3'=>'3','4'=>'4','5'=>'5');
+									echo form_dropdown('quantity_'.$row_counter, $arr_quantity, $row['qty'], array(
+										'class' => 'form-control',
+									));
+									?>												
+									<?php echo form_hidden('rowid_'.$row_counter,$row['rowid']);?>
+								</div>
+								<div class="col-md-2 text-right"><?php echo '<span class="currency" id="INR">&#8377;</span>'.number_format($row['line_total'], 2); ?></div>
 							</div>
 							<?php
 							$row_counter++;
@@ -82,37 +86,38 @@
 					if (sizeof($cartrows) > 0) {
 						?>
 						<button type="submit" class="btn btn-primary">Update Cart</button>
-						<a href="<?php echo site_url('order/init_payment');?>" class="btn btn-primary">Place Order</a>
+						<a href="<?php echo site_url('order/init_payment');?>" class="btn btn-primary d-none">Place Order</a>
 						<?php						
 					}
 					?>
 				</div>
 			</div><!--/.card-footer-->			
-		</div><!--/.card-->
-		<?php echo form_close(); ?>
+		</div><!--/.card-->		
 	</div>
+	
 	<div class="col-md-4">
 		<div class="card">
-			<div class="card-header">Price Details</div>
-			<div class="card-body">
-				<div class="row">
-					<div class="col-md-6">Price(<?php echo isset($total_items) ? $total_items.' items' : ''; ?>)</div>
-					<div class="col-md-6 text-right"><span class="currency" id="INR">&#8377;</span><?php echo isset($cart_total)?number_format($cart_total,2):'';?></div>
-				</div>
-				<div class="row">
-					<div class="col-md-6">Delivery Charges</div>
-					<div class="col-md-6 text-right text-success">FREE</div>
-				</div>
+			<div class="card-header">
+				<div class="text-success">Your order is eligible for FREE Delivery. Select this option at checkout.</div>
 			</div>
-			<div class="card-footer">
-				<div class="row">
-					<h5>Amount Payble</h5>
-					<div class="col-md-6 h4 text-right"><span class="currency" id="INR">&#8377;</span><?php echo isset($cart_total) ? number_format($cart_total,2) :'';?></div>
-				</div>
+			<div class="card-body">
+				<h5>Subtotal(<?php echo isset($total_items) ? $total_items.' items' : ''; ?>): <span class="currency" id="INR">&#8377;</span><?php echo isset($cart_total)?number_format($cart_total,2):'';?></h5>
+			</div>
+			<div class="card-footer text-center">
+				<?php
+					if (sizeof($cartrows) > 0) {
+						?>						
+						<a href="<?php echo site_url('order/init_payment');?>" class="btn btn-primary">Proceed to Checkout</a>
+						<?php						
+					}
+					?>
 			</div>
 		</div>
 		<p>
 			Safe and Secure Payments. Easy returns. 100% Authentic products.
 		</p>
+	</div>
+	</div>
+	<?php echo form_close(); ?>
 	</div>
 </div>
