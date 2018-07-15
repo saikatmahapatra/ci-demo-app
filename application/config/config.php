6f5c0115dadf0080447309527d1a3d0b275b3bc4
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2018-07-15 09:17:08 +0530

    origin url changed

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/ci-demo-app/';
+$config['base_url'] = 'http://localhost/sample-code-igniter-project/';
 
 /*
 |--------------------------------------------------------------------------
