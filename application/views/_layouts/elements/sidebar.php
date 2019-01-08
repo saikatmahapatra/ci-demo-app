b4cc297b0e9ca92dcc9c67bcb41dad162bffe2ba
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2019-01-08 10:16:20 +0530

    UI fixed

--- application/views/_layouts/elements/sidebar.php
+++ application/views/_layouts/elements/sidebar.php
@@ -1,19 +1,20 @@
 <div class="sidebar-overlay" data-toggle="sidebar"></div>
 <aside class="sidebar">
-    <div class="sidebar-user"><img class="sidebar-user-avatar" src="<?php echo base_url('assets/dist/img/default_user@2x.jpg')?>" alt="User Image" />
+    <div class="sidebar-user">
+        <img class="sidebar-user-avatar" src="<?php echo base_url('assets/dist/img/default_user@2x.jpg')?>" alt="User Image" />
         <div class="small">
-            <p class="sidebar-user-name">John Doe</p>
+            <p class="sidebar-user-name">Saikat M</p>
             <p class="sidebar-user-designation">Frontend Developer</p>
         </div>
     </div>
     <ul class="menu">
-        <li><a class="menu-item" href="index.html"><i class="menu-icon fa fa-dashboard"></i><span class="menu-label">Dashboard</span></a></li>
+        <li><a class="menu-item" href="<?php echo base_url('#');?>"><i class="menu-icon fa fa-dashboard"></i><span class="menu-label">Dashboard</span></a></li>
         <li class="treeview"><a class="menu-item" href="#" data-toggle="treeview"><i class="menu-icon fa fa-laptop"></i><span class="menu-label">UI Elements</span><i class="treeview-indicator fa fa-angle-right"></i></a>
             <ul class="treeview-menu">
-                <li><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Bootstrap Elements</a></li>
-                <li><a class="treeview-item" href="#" target="_blank" rel="noopener"><i class="icon fa fa-circle-o"></i> Font Icons</a></li>
-                <li><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Cards</a></li>
-                <li><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Widgets</a></li>
+                <li><a class="treeview-item" href="<?php echo base_url('#');?>"><i class="icon fa fa-circle-o"></i> Bootstrap Elements</a></li>
+                <li><a class="treeview-item" href="<?php echo base_url('#');?>" target="_blank" rel="noopener"><i class="icon fa fa-circle-o"></i> Font Icons</a></li>
+                <li><a class="treeview-item" href="<?php echo base_url('#');?>"><i class="icon fa fa-circle-o"></i> Cards</a></li>
+                <li><a class="treeview-item" href="<?php echo base_url('#');?>"><i class="icon fa fa-circle-o"></i> Widgets</a></li>
             </ul>
         </li>
         
