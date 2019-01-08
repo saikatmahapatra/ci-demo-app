c18c72c2d03b2c76cb74a00d3333e73a2fa26e10
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2019-01-08 10:12:21 +0530

    sibe bar nav bar issue fixed

--- application/views/_layouts/elements/header.php
+++ application/views/_layouts/elements/header.php
@@ -1,33 +1,82 @@
-<header class="header"><a class="header-logo" href="index.html">CI APP</a>
-    <!-- Sidebar toggle button--><a class="sidebar-toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
-    <!-- Navbar Right Menu-->
-    <ul class="nav">
-        <li class="search"><input class="search-input" type="search" placeholder="Search" /><button class="search-button"><i class="fa fa-search"></i></button></li>
-        <!--Notification Menu-->
-        <li class="dropdown"><a class="nav-item" href="#" data-toggle="dropdown" aria-label="Show notifications"><i class="fa fa-bell-o fa-lg"></i></a>
-            <ul class="notification dropdown-menu dropdown-menu-right">
-                <li class="notification-title">You have 4 new notifications.</li>
-                <div class="notification-content">
-                    <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Lisa sent you a mail</p><p class="notification-meta">2 min ago</p></div></a></li>
-                    <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-danger"></i><i class="fa fa-hdd-o fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Mail server not working</p><p class="notification-meta">5 min ago</p></div></a></li>
-                    <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-success"></i><i class="fa fa-money fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Transaction complete</p><p class="notification-meta">2 days ago</p></div></a></li>
-                    <div
-                        class="notification-content">
-                        <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Lisa sent you a mail</p><p class="notification-meta">2 min ago</p></div></a></li>
-                        <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-danger"></i><i class="fa fa-hdd-o fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Mail server not working</p><p class="notification-meta">5 min ago</p></div></a></li>
-                        <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-success"></i><i class="fa fa-money fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Transaction complete</p><p class="notification-meta">2 days ago</p></div></a></li>
-                </div>
-                </div>
-                <li class="notification-footer"><a href="#">See all notifications.</a></li>
+<header class="header">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+        <a class="navbar-brand" href="#">Brand Logo</a>
+        <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button> -->
+        <a class="sidebar-toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarsExampleDefault">
+            <!-- <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown01">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+				<li class="nav-item">
+                    <a class="nav-link" href="#">Link 6</a>
+                </li>
+            </ul> -->
+			
+			  <ul class="navbar-nav">
+                <li class="nav-item search">
+                    <form class="form-inline">
+					  <input class="search-input" type="search" placeholder="Search" aria-label="Search">
+					  <button class="search-button" type="submit"><i class="fa fa-search"></i></button>
+					</form>
+                </li>
+
+				<li class="nav-item dropdown nav-icon-adjust">
+                    <a class="nav-link dropdown-toggle" id="dropdown_notification" href="#" data-toggle="dropdown" aria-label="Show notifications"><i class="fa fa-bell-o fa-lg"></i></a>
+                    <ul class="notification dropdown-menu dropdown-menu-right">
+                        <li class="notification-title">You have 4 new notifications.</li>
+                            <div class="notification-content">
+                                <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Lisa sent you a mail</p><p class="notification-meta">2 min ago</p></div></a></li>
+                                <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-danger"></i><i class="fa fa-hdd-o fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Mail server not working</p><p class="notification-meta">5 min ago</p></div></a></li>
+                                <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-success"></i><i class="fa fa-money fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Transaction complete</p><p class="notification-meta">2 days ago</p></div></a></li>
+                                <div
+                                    class="notification-content">
+                                    <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Lisa sent you a mail</p><p class="notification-meta">2 min ago</p></div></a></li>
+                                    <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-danger"></i><i class="fa fa-hdd-o fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Mail server not working</p><p class="notification-meta">5 min ago</p></div></a></li>
+                                    <li><a class="notification-item" href="javascript:;"><span class="notification-icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-success"></i><i class="fa fa-money fa-stack-1x fa-inverse"></i></span></span><div><p class="notification-message">Transaction complete</p><p class="notification-meta">2 days ago</p></div></a></li>
+                                </div>
+                            </div>
+                        <li class="notification-footer"><a href="#">See all notifications.</a></li>
+                    </ul>
+                </li>
+
+				<li class="nav-item dropdown nav-icon-adjust">
+					<a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true"
+					aria-expanded="false"><i class="fa fa-user fa-lg"></i></a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown03">					
+						<div class="dropdown-item welcome-user-container">									
+							<div class="">John Smith</div>
+							<div class="small">john.smith.developer@example.com</div>
+							<div class="small">Role: Super User</div>
+							<div class="small">Last Login: dd/mm/yyyy hh.mm.ss am/pm</div>
+						</div>					
+						<div class="dropdown-divider mt-3"></div>			
+						<a class="dropdown-item"  href="#">Profile</a>
+						<a class="dropdown-item" href="#">Change Password</a>
+						<a class="dropdown-item" href="#">Signout</a>			
+					</div>
+				</li>
             </ul>
-        </li>
-        <!-- User Menu-->
-        <li class="dropdown"><a class="nav-item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
-            <ul class="dropdown-menu settings-menu dropdown-menu-right">
-                <li><a class="dropdown-item" href="page-user.html"><i class="fa fa-cog fa-lg"></i> Settings</a></li>
-                <li><a class="dropdown-item" href="page-user.html"><i class="fa fa-user fa-lg"></i> Profile</a></li>
-                <li><a class="dropdown-item" href="page-login.html"><i class="fa fa-sign-out fa-lg"></i> Logout</a></li>
-            </ul>
-        </li>
-    </ul>
+        </div>
+    </nav>
 </header>
+
+
--- application/views/_layouts/elements/sidebar.php
+++ application/views/_layouts/elements/sidebar.php
@@ -1,6 +1,6 @@
 <div class="sidebar-overlay" data-toggle="sidebar"></div>
 <aside class="sidebar">
-    <div class="sidebar-user"><img class="sidebar-user-avatar" src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/48.jpg" alt="User Image" />
+    <div class="sidebar-user"><img class="sidebar-user-avatar" src="<?php echo base_url('assets/dist/img/default_user@2x.jpg')?>" alt="User Image" />
         <div class="small">
             <p class="sidebar-user-name">John Doe</p>
             <p class="sidebar-user-designation">Frontend Developer</p>
@@ -10,38 +10,12 @@
         <li><a class="menu-item" href="index.html"><i class="menu-icon fa fa-dashboard"></i><span class="menu-label">Dashboard</span></a></li>
         <li class="treeview"><a class="menu-item" href="#" data-toggle="treeview"><i class="menu-icon fa fa-laptop"></i><span class="menu-label">UI Elements</span><i class="treeview-indicator fa fa-angle-right"></i></a>
             <ul class="treeview-menu">
-                <li><a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle-o"></i> Bootstrap Elements</a></li>
-                <li><a class="treeview-item" href="https://fontawesome.com/v4.7.0/icons/" target="_blank" rel="noopener"><i class="icon fa fa-circle-o"></i> Font Icons</a></li>
-                <li><a class="treeview-item" href="ui-cards.html"><i class="icon fa fa-circle-o"></i> Cards</a></li>
-                <li><a class="treeview-item" href="widgets.html"><i class="icon fa fa-circle-o"></i> Widgets</a></li>
+                <li><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Bootstrap Elements</a></li>
+                <li><a class="treeview-item" href="#" target="_blank" rel="noopener"><i class="icon fa fa-circle-o"></i> Font Icons</a></li>
+                <li><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Cards</a></li>
+                <li><a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Widgets</a></li>
             </ul>
         </li>
-        <li><a class="menu-item" href="charts.html"><i class="menu-icon fa fa-pie-chart"></i><span class="menu-label">Charts</span></a></li>
-        <li class="treeview"><a class="menu-item" href="#" data-toggle="treeview"><i class="menu-icon fa fa-edit"></i><span class="menu-label">Forms</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-            <ul class="treeview-menu">
-                <li><a class="treeview-item" href="form-components.html"><i class="icon fa fa-circle-o"></i> Form Components</a></li>
-                <li><a class="treeview-item" href="form-custom.html"><i class="icon fa fa-circle-o"></i> Custom Components</a></li>
-                <li><a class="treeview-item" href="form-samples.html"><i class="icon fa fa-circle-o"></i> Form Samples</a></li>
-                <li><a class="treeview-item" href="form-notifications.html"><i class="icon fa fa-circle-o"></i> Form Notifications</a></li>
-            </ul>
-        </li>
-        <li class="treeview"><a class="menu-item" href="#" data-toggle="treeview"><i class="menu-icon fa fa-th-list"></i><span class="menu-label">Tables</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-            <ul class="treeview-menu">
-                <li><a class="treeview-item" href="table-basic.html"><i class="icon fa fa-circle-o"></i> Basic Tables</a></li>
-                <li><a class="treeview-item" href="table-data-table.html"><i class="icon fa fa-circle-o"></i> Data Tables</a></li>
-            </ul>
-        </li>
-        <li class="treeview"><a class="menu-item" href="#" data-toggle="treeview"><i class="menu-icon fa fa-file-text"></i><span class="menu-label">Pages</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-            <ul class="treeview-menu">
-                <li><a class="treeview-item" href="blank-page.html"><i class="icon fa fa-circle-o"></i> Blank Page</a></li>
-                <li><a class="treeview-item" href="page-login.html"><i class="icon fa fa-circle-o"></i> Login Page</a></li>
-                <li><a class="treeview-item" href="page-lockscreen.html"><i class="icon fa fa-circle-o"></i> Lockscreen Page</a></li>
-                <li><a class="treeview-item" href="page-user.html"><i class="icon fa fa-circle-o"></i> User Page</a></li>
-                <li><a class="treeview-item" href="page-invoice.html"><i class="icon fa fa-circle-o"></i> Invoice Page</a></li>
-                <li><a class="treeview-item" href="page-calendar.html"><i class="icon fa fa-circle-o"></i> Calendar Page</a></li>
-                <li><a class="treeview-item" href="page-mailbox.html"><i class="icon fa fa-circle-o"></i> Mailbox</a></li>
-                <li><a class="treeview-item" href="page-error.html"><i class="icon fa fa-circle-o"></i> Error Page</a></li>
-            </ul>
-        </li>
+        
     </ul>
 </aside>
