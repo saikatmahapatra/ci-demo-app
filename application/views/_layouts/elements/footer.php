f82dfa41313cf1e4e0172399a1abe2c35ef38acf
Saikat <mahapatra.saikat@gmail.com>
2019-01-09 18:46:43 +0530

    ui

--- application/views/_layouts/elements/footer.php
+++ application/views/_layouts/elements/footer.php
@@ -1,4 +1,4 @@
-<div class="text-muted my-2 text-right">
+<div class="text-muted my-2 text-center">
     <div class="">
         <?php echo $this->config->item('app_admin_copy_right');?>
     </div>
