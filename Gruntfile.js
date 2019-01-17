e58af8fb1fec44e9a21ec0e8d50392c6fd832195
Saikat Mahapatra <mahapatra.saikat@gmail.com>
2019-01-17 23:34:46 +0530

    grunt file cleaned up and updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,138 +1,129 @@
-// CI App Gruntfile
-module.exports = function (grunt) { // jshint ignore:line
-  'use strict'
+module.exports = function(grunt) {
+    'use strict'
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    watch: {
-      /*less: {
-        // Compiles less files upon saving
-        files: ['assets/src/less/*.less'],
-        tasks: ['less:development', 'less:production', 'notify:less']
-      },*/
-	  sass : {        
-        files: ['assets/src/sass/*.scss'],
-        tasks: ['sass','notify:sass']
-      },
-      js: {
-        // Compile js files upon saving
-        files: ['assets/src/js/*.js'],
-        tasks: ['js', 'notify:js']
-      }
-    },
-    // Notify end of tasks
-    notify: {
-      less: {
-        options: {
-          title: 'CI App',
-          message: 'LESS finished running'
-        }
-      },
-	  sass: {
-        options: {
-          title  : 'My App',
-          message: 'Ok. SASS done'
-        }
-      },
-      js: {
-        options: {
-          title: 'CI App',
-          message: 'JS bundler finished running'
-        }
-      }
-    },
-    // 'less'-task configuration
-    // This task will compile all less files upon saving to create both CI App.css and CI App.min.css
-    less: {
-      // Development not compressed
-      development: {
-        files: {
-          // compilation.css  :  source.less
-          'assets/dist/css/styles.css': 'assets/src/less/styles.less',
-          'assets/dist/css/admin.css': 'assets/src/less/admin.less'
-        }
-      },
-      // Production compressed version
-      production: {
-        options: {
-          compress: true
+        watch: {
+            // less : {        
+            // files: ['assets/src/less/**/*.less'],
+            // tasks: ['less:development', 'less:production','notify:less']
+            // },
+            js: {
+                files: ['assets/src/js/**/*.js'],
+                tasks: ['js', 'notify:js']
+            },
+            sass: {
+                files: ['assets/src/sass/**/*.scss'],
+                tasks: ['sass', 'notify:sass']
+            },
         },
-        files: {
-          // compilation.css  :  source.less
-          'assets/dist/css/styles.min.css': 'assets/src/less/styles.less',
-          'assets/dist/css/admin.min.css': 'assets/src/less/admin.less'
-        }
-      }
-    },
 
-    //Copy files
-    copy: {
-      main: {
-        expand: false,
-        src: 'assets/src/js/*.js',
-        dest: 'assets/dist/js/',
-      },
-    },
+        notify: {
+            less: {
+                options: {
+                    title: 'My App',
+                    message: 'Ok. LESS done'
+                }
+            },
+            sass: {
+                options: {
+                    title: 'My App',
+                    message: 'Ok. SASS done'
+                }
+            },
+            js: {
+                options: {
+                    title: 'My App',
+                    message: 'Ok. JS done'
+                }
+            },
+            svgcss: {
+                options: {
+                    title: 'My App',
+                    message: 'Ok. SVG to CSS done'
+                }
+            }
+        },
 
-    //Copy 
-    copy: {
-      main: {
-        expand: true,
-        cwd: 'assets/src/js/',
-        src: '**',
-        dest: 'assets/dist/js/',
-        flatten: true,
-        filter: 'isFile',
-      },
-    },
+        svgcss: {
+            toCrlf: {
+                options: {
+                    eol: 'crlf',
+                    cssprefix: 'sm-icon',
+                    previewhtml: 'svg-icon-test.html'
+                },
+                files: {
+                    'assets/dist/css/svg_to_css.css': ['assets/src/img/**/*.svg']
+                }
+            }
+        },
 
-    // Uglify task info. Compress the js files.
-    uglify: {
-      options: {
-        mangle: true,
-        preserveComments: 'some'
-      },
-      production: {
-        files: {          
-          'assets/dist/js/app.min.js': ['assets/src/js/app.js'],
-        }
-      }
-    },
+        less: {
+            development: {
+                files: {
+                    'assets/dist/css/styles.css': 'assets/src/less/styles.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    'assets/dist/css/styles.min.css': 'assets/src/less/styles.less'
+                }
+            }
+        },
 
-    // Concatenate JS Files
-    concat: {
-      options: {
-        separator: '\n\n',
-        banner: '/*! \n'
-          //+ '* \n'
-          //+ '* ================\n'
-          //+ '* Main JS application file for CI App v2. This file\n'
-          //+ '* should be included in all pages. It controls some layout\n'
-          //+ '* options and implements exclusive CI App plugins.\n'
-          //+ '*\n'
-          + '* @Author  Saikat Mahapatra\n'
-          + '* @Support \n'
-          + '* @Email   <mahapatra.saikat29@gmail.com>\n'
-          + '* @version <%= pkg.version %>\n'
-          + '* @repository <%= pkg.repository.url %>\n'
-          + '* @license MIT <http://opensource.org/licenses/MIT>\n'
-          + '*/\n\n'
-        //+ '// Make sure jQuery has been loaded\n'
-        //+ 'if (typeof jQuery === \'undefined\') {\n'
-        //+ 'throw new Error(\'CI App requires jQuery\')\n'
-        //+ '}\n\n'
-      },
-      dist: {
-        src: ['assets/src/js/app.js'],
-        dest: 'assets/dist/js/app.js'
-      }
-    },
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'assets/src/js/',
+                src: '**',
+                dest: 'assets/dist/js/',
+                flatten: true,
+                filter: 'isFile',
+            },
+        },
 
-    // Replace image paths in CI App without plugins
-    /*replace: {
+        uglify: {
+            options: {
+                mangle: true,
+                preserveComments: 'some'
+            },
+            production: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/src/js',
+                    src: '**/*.js',
+                    dest: 'assets/dist/js',
+                    ext: '.min.js'
+                }]
+            }
+        },
+
+        concat: {
+            options: {
+                separator: '\n\n',
+                banner: '/*! \n' +
+                    '* ================\n' +
+                    '* Main JS file of the application having common functions\n' +
+                    '*\n' +
+                    '* @Author <%= pkg.author %>\n' +
+                    '* @version <%= pkg.version %>\n' +
+                    '* @repository <%= pkg.repository.url %>\n' +
+                    '* @license <%= pkg.license %>\n' +
+                    '*/\n\n'
+            },
+            dist: {
+                src: ['assets/src/js/app.js'],
+                dest: 'assets/dist/js/app.js'
+            }
+        },
+
+        /*replace: {
       withoutPlugins   : {
-        src         : ['assets/dist/css/alt/CI App-without-plugins.css'],
-        dest        : 'assets/dist/css/alt/CI App-without-plugins.css',
+        src         : ['assets/dist/css/alt/My App-without-plugins.css'],
+        dest        : 'assets/dist/css/alt/My App-without-plugins.css',
         replacements: [
           {
             from: '../img',
@@ -141,8 +132,8 @@ module.exports = function (grunt) { // jshint ignore:line
         ]
       },
       withoutPluginsMin: {
-        src         : ['assets/dist/css/alt/CI App-without-plugins.min.css'],
-        dest        : 'assets/dist/css/alt/CI App-without-plugins.min.css',
+        src         : ['assets/dist/css/alt/My App-without-plugins.min.css'],
+        dest        : 'assets/dist/css/alt/My App-without-plugins.min.css',
         replacements: [
           {
             from: '../img',
@@ -150,10 +141,9 @@ module.exports = function (grunt) { // jshint ignore:line
           }
         ]
       }
-    },*/
-
-    // Build the documentation files
-    /*includes: {
+    },
+	
+	includes: {
       build: {
         src    : ['*.html'], // Source files
         dest   : 'documentation/', // Destination directory
@@ -166,141 +156,127 @@ module.exports = function (grunt) { // jshint ignore:line
       }
     },*/
 
-    // Optimize images
-    image: {
-      dynamic: {
-        files: [
-          {
-            expand: true,
-            cwd: 'assets/src/img/',
-            src: ['**/*.{png,jpg,gif,svg,jpeg}'],
-            dest: 'assets/dist/img/'
-          }
-        ]
-      }
-    },
-
-    // Validate JS code
-    jshint: {
-      options: {
-        jshintrc: 'assets/src/js/.jshintrc'
-      },
-      grunt: {
-        options: {
-          jshintrc: 'assets/src/grunt/.jshintrc'
+        image: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/src/img/',
+                    src: ['**/*.{png,jpg,gif,svg,jpeg,ico}'],
+                    dest: 'assets/dist/img/'
+                }]
+            }
         },
-        src: 'Gruntfile.js'
-      },
-      core: {
-        src: 'assets/src/js/*.js'
-      }
-    },
 
-    jscs: {
-      options: {
-        config: 'assets/src/js/.jscsrc'
-      },
-      core: {
-        src: '<%= jshint.core.src %>'
-      }
-    },
+        jshint: {
+            options: {
+                jshintrc: 'assets/src/js/.jshintrc'
+            },
+            grunt: {
+                options: {
+                    jshintrc: 'assets/src/grunt/.jshintrc'
+                },
+                src: 'Gruntfile.js'
+            },
+            core: {
+                src: 'assets/src/js/*.js'
+            }
+        },
 
-    // Validate CSS files
-    csslint: {
-      options: {
-        csslintrc: 'assets/src/less/.csslintrc'
-      },
-      dist: [
-        'assets/dist/css/styles.css'
-      ]
-    },
+        jscs: {
+            options: {
+                config: 'assets/src/js/.jscsrc'
+            },
+            core: {
+                src: '<%= jshint.core.src %>'
+            }
+        },
 
-    // Validate Bootstrap HTML
-    //bootlint: {
-    //options: {
-    //relaxerror: ['W005']
-    //},
-    //files  : ['pages/**/*.html', '*.html']
-    // },
+        csslint: {
+            options: {
+                csslintrc: 'assets/src/less/.csslintrc'
+            },
+            dist: [
+                'assets/dist/css/styles.css'
+            ]
+        },
 
-    // Delete images in build directory
-    // After compressing the images in the build/img dir, there is no need
-    // for them
-    clean: {
-      build: ['assets/src/img/*']
-    },
-	
-	// SASS to CSS
-	sass: {
-		dist: {
-			options: {
-				outputStyle: 'expanded',
-				sourceMap: false
-			},
-			files: [{
-				expand: true,
-				cwd: 'assets/src/sass/',
-				src: ['*.scss'],
-				dest: 'assets/dist/css/',
-				ext: '.css'
-			}]
-		}
-	},
-	
-	// Post Compilation of CSS
-	postcss: {
-		options: {
-			map: false,
-			processors: [
-				require('autoprefixer')({browsers: 'last 3 versions'})
-			]
-		},
-		dist: {
-			src: ['assets/dist/css/*.css']
-		}
-	}
-  })
+        // Validate Bootstrap HTML
+        //bootlint: {
+        //options: {
+        //relaxerror: ['W005']
+        //},
+        //files  : ['pages/**/*.html', '*.html']
+        // },
 
-  // Load all grunt tasks
+        clean: {
+            build: ['assets/src/img/*']
+        },
 
-  // LESS Compiler
-  grunt.loadNpmTasks('grunt-contrib-less');
-  //SASS Compiler
-  grunt.loadNpmTasks('grunt-sass');
-  //Post Compilation of CSS
-  grunt.loadNpmTasks('grunt-postcss');
-  // Watch File Changes
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  // Compress JS Files
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  // Include Files Within HTML
-  grunt.loadNpmTasks('grunt-includes');
-  // Optimize images
-  grunt.loadNpmTasks('grunt-image');
-  // Validate JS code
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-jscs');
-  // Delete not needed files
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  // Lint CSS
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-  // Lint Bootstrap
-  grunt.loadNpmTasks('grunt-bootlint');
-  // Concatenate JS files
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  // Notify
-  grunt.loadNpmTasks('grunt-notify');
-  // Replace
-  grunt.loadNpmTasks('grunt-text-replace');
-  // Copy Files
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  // Linting task
-  grunt.registerTask('lint', ['jshint', 'csslint', 'bootlint']);
-  // JS task
-  grunt.registerTask('js', ['copy', 'concat', 'uglify']);
-  // CSS Task
-  grunt.registerTask('css', ['image', 'sass','postcss']);
+        sass: {
+            dist: {
+                options: {
+                    outputStyle: 'expanded',
+                    sourceMap: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'assets/src/sass/',
+                    src: ['**/*.scss'],
+                    dest: 'assets/dist/css/',
+                    ext: '.css'
+                }]
+            }
+        },
 
-  // The default task (running 'grunt' in console) is 'watch'
-  grunt.registerTask('default', ['watch']);
+        postcss: {
+            options: {
+                map: false,
+                processors: [
+                    require('autoprefixer')({ browsers: 'last 3 versions' })
+                ]
+            },
+            dist: {
+                src: ['assets/dist/css/**/*.css']
+            }
+        },
+
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/dist/css',
+                    src: ['**/*.css', '!*.min.css'],
+                    dest: 'assets/dist/css',
+                    ext: '.min.css'
+                }]
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-includes');
+    grunt.loadNpmTasks('grunt-image');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-bootlint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-notify');
+    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-svg-css');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('lint', ['jshint', 'csslint', 'bootlint']);
+    grunt.registerTask('img', ['image']);
+    grunt.registerTask('js', ['copy', 'concat', 'uglify']);
+    grunt.registerTask('css', ['sass', 'postcss', 'cssmin', 'image']);
+    grunt.registerTask('default', ['watch']);
+
 }
